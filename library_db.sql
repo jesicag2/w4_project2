abcb010f5aba72b8ff6b223cdff375bb56634d76
jesicag2 <jesicagarcia109@gmail.com>
2024-04-17 22:35:28 -0400

    added sql file

---
+++ library_db.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE library_db;
+
+USE library_db;
+
+CREATE TABLE authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    biography TEXT
+);
+
+CREATE TABLE books(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author INT,
+    isbn VARCHAR(13) NOT NULL,
+    genre INT,
+    publication_date DATE,
+    availability BOOLEAN DEFAULT 1,
+    FOREIGN KEY (author) REFERENCES authors(id)
+    
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    library_id VARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE borrowed_books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    borrow_date DATE NOT NULL,
+    return_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
